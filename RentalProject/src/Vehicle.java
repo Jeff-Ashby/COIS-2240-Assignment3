cfe3ea92261f7db2932ec0f7f0ed7f895087e9ad
Jeff-Ashby <jeff.ashby2@gmail.com>
2025-11-27 19:38:27 -0500

    Refactored a method called capitalize() to reduce code duplication
    Made changes to the constructor in Vehicle.java to have reduced code
    duplication and increased clarity.

--- RentalProject/src/Vehicle.java
+++ RentalProject/src/Vehicle.java
@@ -8,20 +8,20 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
+    	this.make = capitalize(make);
+    	this.model = capitalize(model);
     	
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
     }
+
+	private String capitalize(String input) { // new method to reduce redundancy
+		if (input == null || input.isEmpty())
+    		return null;
+    	else
+    		return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+	}
 
     public Vehicle() {
         this(null, null, 0);
