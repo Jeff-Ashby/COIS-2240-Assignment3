c4e810d41b4fb0360f39241b6e26cd6769a0da63
Jeff-Ashby <jeff.ashby2@gmail.com>
2025-11-30 22:08:51 -0500

    added one last test method for testing the singleton functionality.

--- RentalProject/src/VehicleRentalTest.java
+++ RentalProject/src/VehicleRentalTest.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+import java.lang.reflect.Constructor; // import for singleton testing.
+import java.lang.reflect.Modifier;
+import java.time.LocalDate;
 class VehicleRentalTest {
 
 	@Test
@@ -32,22 +34,37 @@ class VehicleRentalTest {
 		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Available, "Vehicle is not set to available");
 		
 		// test to see if vehicle is able to be rented
-		assertTrue(testSystem.rentVehicle(testVehicle, testCustomer, null, 0), "Vehicle not successfully rented.");
+		assertTrue(testSystem.rentVehicle(testVehicle, testCustomer, LocalDate.now(), 0), "Vehicle not successfully rented.");
 		
 		// test to see if vehicle is now considered rented
 		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Rented, "Vehicle is not set to rented");
 
 		// test to see if you can rent the vehicle test (should be false)
-		assertFalse(testSystem.rentVehicle(testVehicle, testCustomer, null, 0), "Vehicle is not properly rented");
+		assertFalse(testSystem.rentVehicle(testVehicle, testCustomer, LocalDate.now(), 0), "Vehicle is not properly rented");
 		
 		// test to see if the return worked
-		assertTrue(testSystem.returnVehicle(testVehicle, testCustomer, null, 0), "Vehicle failed to be returned."); 
+		assertTrue(testSystem.returnVehicle(testVehicle, testCustomer, LocalDate.now(), 0), "Vehicle failed to be returned."); 
 		
 		// test to see if vehicle is now available
 		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Available, "Vehicle is not available");
 
 		// test to see if you can return the vehicle again
-		assertFalse(testSystem.returnVehicle(testVehicle, testCustomer, null, 0), "Vehicle was returned when available (error).");
+		assertFalse(testSystem.returnVehicle(testVehicle, testCustomer, LocalDate.now(), 0), "Vehicle was returned when available (error).");
+	}
+	
+	@Test
+	void testSingletonRentalSystem() {
+		try {
+			Constructor<RentalSystem> constructor = RentalSystem.class.getDeclaredConstructor();
+			assertTrue(constructor.getModifiers() == Modifier.PRIVATE, "Constructor is not private.");
+			assertTrue(RentalSystem.getInstance() != null, "Get instance method returns null");
+			
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
