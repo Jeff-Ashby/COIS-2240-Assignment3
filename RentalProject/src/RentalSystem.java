76b24753a3a90870ce603fe795935d6ed246102f
Jeff-Ashby <jeff.ashby2@gmail.com>
2025-11-30 21:59:52 -0500

    Added a new testing method to test renting and returning vehicles.
    This required me to change logic within the RentalSystem.Java file,
    having the rent and return methods return booleans rather than be voids.

--- RentalProject/src/RentalSystem.java
+++ RentalProject/src/RentalSystem.java
@@ -76,7 +76,7 @@ public class RentalSystem {
 		}
 	}
 
-	public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
+	public boolean rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
 		if (vehicle.getStatus() == Vehicle.VehicleStatus.Available) {
 			vehicle.setStatus(Vehicle.VehicleStatus.Rented);
 			// create record object, then add it and save it to the .txt file
@@ -84,12 +84,14 @@ public class RentalSystem {
 			rentalHistory.addRecord(record);
 			this.saveRecord(record);
 			System.out.println("Vehicle rented to " + customer.getCustomerName());
+			return true;
 		} else {
 			System.out.println("Vehicle is not available for renting.");
+			return false;
 		}
 	}
 
-	public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
+	public boolean returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
 		if (vehicle.getStatus() == Vehicle.VehicleStatus.Rented) {
 			vehicle.setStatus(Vehicle.VehicleStatus.Available);
 			// create record object, then add it and save it to the .txt file
@@ -97,8 +99,10 @@ public class RentalSystem {
 			rentalHistory.addRecord(record);
 			this.saveRecord(record);
 			System.out.println("Vehicle returned by " + customer.getCustomerName());
+			return true;
 		} else {
 			System.out.println("Vehicle is not rented.");
+			return false;
 		}
 	}
 
--- RentalProject/src/VehicleRentalTest.java
+++ RentalProject/src/VehicleRentalTest.java
@@ -19,5 +19,35 @@ class VehicleRentalTest {
 		assertThrows(IllegalArgumentException.class, () -> test.setLicensePlate("AAA1000"));
 		assertThrows(IllegalArgumentException.class, () -> test.setLicensePlate("ZZZ99"));
 	}
+	
+	@Test
+	void testRentAndReturnVehicle() {
+		// instantiate test objects
+		Vehicle testVehicle = new Car("Test Model", "Test Company", 2025, 4); 
+		Customer testCustomer = new Customer(001, "Jane");
+		RentalSystem testSystem = RentalSystem.getInstance();
+		
+		
+		// test to see if vehicle is initially available
+		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Available, "Vehicle is not set to available");
+		
+		// test to see if vehicle is able to be rented
+		assertTrue(testSystem.rentVehicle(testVehicle, testCustomer, null, 0), "Vehicle not successfully rented.");
+		
+		// test to see if vehicle is now considered rented
+		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Rented, "Vehicle is not set to rented");
+
+		// test to see if you can rent the vehicle test (should be false)
+		assertFalse(testSystem.rentVehicle(testVehicle, testCustomer, null, 0), "Vehicle is not properly rented");
+		
+		// test to see if the return worked
+		assertTrue(testSystem.returnVehicle(testVehicle, testCustomer, null, 0), "Vehicle failed to be returned."); 
+		
+		// test to see if vehicle is now available
+		assertTrue(testVehicle.getStatus() == Vehicle.VehicleStatus.Available, "Vehicle is not available");
+
+		// test to see if you can return the vehicle again
+		assertFalse(testSystem.returnVehicle(testVehicle, testCustomer, null, 0), "Vehicle was returned when available (error).");
+	}
 
 }
